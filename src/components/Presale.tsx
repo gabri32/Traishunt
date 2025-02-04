ee14ebfc3f5b13ad87fdb529a481672f35823a03
gabri32 <gabodelac@gmail.com>
2025-02-03 21:47:55 -0500

    cambio confirmar

--- src/components/Presale.tsx
+++ src/components/Presale.tsx
@@ -12,7 +12,7 @@ interface Props {
 const Component: React.FC<Props> = ({ expired }: Props) => {
 
   let VITE_ABI_USDT = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "userAddress", "type": "address" }, { "indexed": false, "internalType": "address payable", "name": "relayerAddress", "type": "address" }, { "indexed": false, "internalType": "bytes", "name": "functionSignature", "type": "bytes" }], "name": "MetaTransactionExecuted", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "bytes32", "name": "previousAdminRole", "type": "bytes32" }, { "indexed": true, "internalType": "bytes32", "name": "newAdminRole", "type": "bytes32" }], "name": "RoleAdminChanged", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" }], "name": "RoleGranted", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" }], "name": "RoleRevoked", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "inputs": [], "name": "CHILD_CHAIN_ID", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "CHILD_CHAIN_ID_BYTES", "outputs": [{ "internalType": "bytes", "name": "", "type": "bytes" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "DEFAULT_ADMIN_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "DEPOSITOR_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "ERC712_VERSION", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "ROOT_CHAIN_ID", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "ROOT_CHAIN_ID_BYTES", "outputs": [{ "internalType": "bytes", "name": "", "type": "bytes" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "approve", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "name_", "type": "string" }], "name": "changeName", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "subtractedValue", "type": "uint256" }], "name": "decreaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }, { "internalType": "bytes", "name": "depositData", "type": "bytes" }], "name": "deposit", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "userAddress", "type": "address" }, { "internalType": "bytes", "name": "functionSignature", "type": "bytes" }, { "internalType": "bytes32", "name": "sigR", "type": "bytes32" }, { "internalType": "bytes32", "name": "sigS", "type": "bytes32" }, { "internalType": "uint8", "name": "sigV", "type": "uint8" }], "name": "executeMetaTransaction", "outputs": [{ "internalType": "bytes", "name": "", "type": "bytes" }], "stateMutability": "payable", "type": "function" }, { "inputs": [], "name": "getChainId", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "pure", "type": "function" }, { "inputs": [], "name": "getDomainSeperator", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }], "name": "getNonce", "outputs": [{ "internalType": "uint256", "name": "nonce", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }], "name": "getRoleAdmin", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "uint256", "name": "index", "type": "uint256" }], "name": "getRoleMember", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }], "name": "getRoleMemberCount", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "grantRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "hasRole", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "addedValue", "type": "uint256" }], "name": "increaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "name_", "type": "string" }, { "internalType": "string", "name": "symbol_", "type": "string" }, { "internalType": "uint8", "name": "decimals_", "type": "uint8" }, { "internalType": "address", "name": "childChainManager", "type": "address" }], "name": "initialize", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "renounceRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "revokeRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transfer", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "withdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
-  let VITE_CONTRACT_ABI = [{"inputs":[{"internalType":"address","name":"_usdtToken","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"components":[{"internalType":"address","name":"wallet","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"internalType":"struct USDTManager.Payment[]","name":"payments","type":"tuple[]"}],"name":"distributeUSDT","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"usdtToken","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdrawUSDT","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+  let VITE_CONTRACT_ABI = [{"inputs":[{"internalType":"address","name":"_usdtToken","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"allowance","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"totalAmount","type":"uint256"}],"name":"LogAllowanceAndAmount","type":"event"},{"inputs":[{"components":[{"internalType":"address","name":"wallet","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"internalType":"struct USDTManager.Payment[]","name":"payments","type":"tuple[]"}],"name":"distributeUSDT","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"usdtToken","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdrawUSDT","outputs":[],"stateMutability":"nonpayable","type":"function"}]
   const [tsh, setTsh] = useState(0);
   const [collected, setCollected] = useState<number>(0);
   const total = 150000;
@@ -58,7 +58,7 @@ const Component: React.FC<Props> = ({ expired }: Props) => {
       setLoading(true);
    //   setError(null);
       const result = await checkBalance(inputValue);
-      const calculatedTsh = Number(result.valores.precioTotal.toFixed(6));
+      const calculatedTsh = Number(result.valores.precioTotal.toFixed(2));
       setTsh(calculatedTsh);
       console.log(tieneref) 
       setBalanceInfo(result);
@@ -107,7 +107,9 @@ const Component: React.FC<Props> = ({ expired }: Props) => {
       }
   
       let nuevoReferido = referido ;  // Si hay un referido en el estado, lo usamos
-
+   if (referido === "") {
+        nuevoReferido = "67a15ad5432d9c4ec2cf3fed"
+   }
       const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
   
       if (accounts.length === 0) {
@@ -206,59 +208,74 @@ const Component: React.FC<Props> = ({ expired }: Props) => {
   
 
   const approveTokenFrontend = async () => {
-    if (usdtBalance > 0) {
-      const usdtAddress = import.meta.env.VITE_USDT_ADDRESS;
-      const abiUsdt = VITE_ABI_USDT;
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      await provider.send("eth_requestAccounts", []); 
-      const signer = await provider.getSigner();
-      const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
-      const abiContract = VITE_CONTRACT_ABI;
-      const tokenUsdt = new ethers.Contract(usdtAddress, abiUsdt, signer);
-      const contract = new ethers.Contract(contractAddress, abiContract, signer);
-  
-      const amountToApprove = tsh * 1e6;
-      const params = {
-        cantidadTokens: amountToBuy,
-        wallet: walletAddress,
-      };
-  
-      try {
-        if (typeof maticBalance === "number" && !isNaN(maticBalance) && maticBalance > 1) {
-          console.log("Iniciando la aprobación de tokens...");
-          const txApprove = await tokenUsdt.approve(contractAddress, amountToApprove);
-          const receiptApprove = await txApprove.wait();
-          console.log("Transacción confirmada para approve:", receiptApprove);
-  
-          console.log("Realizando la compra...");
-          const compra = await buyTokens(params);
-          const payments = compra.costo.wallettocontrato;
-  
-          console.log("Distribuyendo USDT...",compra);
-          const txDistribute = await contract.distributeUSDT(payments);
-          const receiptDistribute = await txDistribute.wait();
-          console.log("Transacción confirmada para distributeUSDT:", receiptDistribute);
-          console.log("Transacción completada con éxito");
-          let params1 = { wallet: walletAddress,
-            cantidadTokens: params.cantidadTokens,
-            precioTotal: amountToBuy,}
-            await confirmarCompra(params1)
-            .then((res) => {
-              console.log("Éxito",res);
-            })
-            .catch((error) => {
-              console.error("Error en la compra:", error);
-            });
-          alert("Transacción completada con éxito");
-        } else {
-          alert("No tienes suficiente MATIC para realizar la transacción");
+    console.log("tshhhhhhhhh", tsh);
+    console.log("usdtBalance", usdtBalance);
+
+    if (usdtBalance > tsh * 1e6) {
+        const usdtAddress = import.meta.env.VITE_USDT_ADDRESS;
+        const abiUsdt = VITE_ABI_USDT;
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = await provider.getSigner();
+        const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
+        const abiContract = VITE_CONTRACT_ABI;
+        const tokenUsdt = new ethers.Contract(usdtAddress, abiUsdt, signer);
+        const contract = new ethers.Contract(contractAddress, abiContract, signer);
+
+        const amountToApprove = tsh * 1e6;
+        const params = {
+            cantidadTokens: amountToBuy,
+            wallet: walletAddress,
+        };
+
+        try {
+          console.log("valor aporbar",amountToApprove);
+            if (typeof maticBalance === "number" && !isNaN(maticBalance) && maticBalance > 1) {
+                console.log("Iniciando la aprobación de tokens...");
+                const txApprove = await tokenUsdt.approve(contractAddress, amountToApprove);
+                const receiptApprove = await txApprove.wait();
+                console.log("Transacción confirmada para approve:", receiptApprove);
+
+                console.log("Realizando la compra...");
+                const compra = await buyTokens(params);
+                const payments = compra.costo.wallettocontrato;
+console.log("payments",payments[0].amount);
+console.log("amountToApprove",amountToApprove);
+                if (amountToApprove >= payments[0].amount) { // Comparación agregada
+                    console.log("Distribuyendo USDT...", compra);
+                    const txDistribute = await contract.distributeUSDT(payments);
+                    const receiptDistribute = await txDistribute.wait();
+                    console.log("Transacción confirmada para distributeUSDT:", receiptDistribute);
+                    console.log("Transacción completada con éxito");
+
+                    let params1 = {
+                        wallet: walletAddress,
+                        cantidadTokens: params.cantidadTokens,
+                        precioTotal: amountToBuy,
+                    };
+
+                    await confirmarCompra(params1)
+                        .then((res) => {
+                            console.log("Éxito", res);
+                        })
+                        .catch((error) => {
+                            console.error("Error en la compra:", error);
+                        });
+
+                    alert("Transacción completada con éxito");
+                } else {
+                    alert("El monto aprobado no es suficiente para la transacción");
+                }
+            } else {
+                alert("No tienes suficiente MATIC para realizar la transacción");
+            }
+        } catch (error) {
+            console.error("Error en el flujo de ejecución:", error);
         }
-      } catch (error) {
-        console.error("Error en el flujo de ejecución:", error);
-      }
+    } else {
+        alert("No tienes suficiente USDT para realizar la transacción");
     }
-  };
-  
+};
 
 
 
