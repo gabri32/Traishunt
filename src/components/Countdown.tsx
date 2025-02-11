aaec138e931571221b6e3045b6020880c87967c8
gabri32 <gabodelac@gmail.com>
2025-02-10 23:14:30 -0500

    contador

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 interface Props {
   expired: Date
 }
 
 const Component: React.FC<Props> = ({ expired }: Props) => {
-  const [ days, setDays ] = useState(0)
-  const [ hours, setHours ] = useState(0)
-  const [ minutes, setMinutes ] = useState(0)
-  const [ seconds, setSeconds ] = useState(0)
-  const [ showSeconds, setShowSeconds ] = useState(false)
+  // const [ days, setDays ] = useState(0)
+  // const [ hours, setHours ] = useState(0)
+  // const [ minutes, setMinutes ] = useState(0)
+  // const [ seconds, setSeconds ] = useState(0)
+  // const [ showSeconds, setShowSeconds ] = useState(false)
 
   
   useEffect(() => {
